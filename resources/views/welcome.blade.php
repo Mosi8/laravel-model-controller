27de9acd30432bf11c963f78018eef82fe407085
Mosi8 <mauriziomosi8@gmail.com>
2022-03-06 15:51:15 +0100

    completo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Film</title>
 </head>
 <body>
-    @foreach ($ls_film as $chiave => $film)
-            <div>
-                <h4>Titolo : {{$film['title']}}</h4>
-                <p>Titolo originale : {{$film['original_title']}}</p>
-            </div>
-    @endforeach
+    <div class="container">
+        <div class="row justify-content-center mt-3">
+            @foreach ($ls_film as $chiave => $film)
+                <div class="col-3 g-3 m-2 carte">
+                    <p>Titolo: <span>{{$film['title']}}</span></p>
+                    <p>Titolo originale: <span>{{$film['original_title']}}</span></p>
+                    <p>Voto: <span>{{$film['vote']}}</span></p>
+                </div>
+             @endforeach
+        </div>
+    </div>
+    
 </body>
 </html>
