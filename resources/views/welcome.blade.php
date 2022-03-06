260298a394a9febc8bc4c23fd9128ddf024c62bf
Mosi8 <mauriziomosi8@gmail.com>
2022-03-06 15:32:17 +0100

    completo senza bonus

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,17 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    @foreach ($ls_film as $chiave => $film)
+            <div>
+                <h4>Titolo : {{$film['title']}}</h4>
+                <p>Titolo originale : {{$film['original_title']}}</p>
+            </div>
+    @endforeach
+</body>
+</html>
